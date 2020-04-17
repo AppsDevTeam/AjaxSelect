1122476e941c9c5709163fbd15fa8754d60a8a5a
Michal Lohniský <michal@appsdevteam.com>
2020-04-17 10:39:46 +0200

    Notice when throwing an exception
    Values can be an array.

--- src/Components/AjaxSelect/Traits/InvalidSetValueTrait.php
+++ src/Components/AjaxSelect/Traits/InvalidSetValueTrait.php
@@ -50,7 +50,7 @@ trait InvalidSetValueTrait {
 			case AjaxSelect\DI\AjaxSelectExtension::INVALID_VALUE_MODE_EXCEPTION:
 				$set = \Nette\Utils\Strings::truncate(implode(', ', array_map(function ($s) { return var_export($s, TRUE); }, array_keys($this->items))), 70, '...');
 
-				throw new \Nette\InvalidArgumentException("Value '$values' is out of allowed set [$set] in field '{$this->name}'.");
+				throw new \Nette\InvalidArgumentException("Value '". (is_array($values) ? implode("', '", $values) : $values) ."' is out of allowed set [$set] in field '{$this->name}'.");
 
 			case AjaxSelect\DI\AjaxSelectExtension::INVALID_VALUE_MODE_EMPTY:
 				return $this instanceof AjaxSelect\Interfaces\IMultiSelectControl
